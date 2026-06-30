deec7737bfb3d53789d504fd67aa697f16f45d31
kittyruoge <grdinag785@icloud.com>
2026-06-30 18:46:43 +0800

    Add files via upload

---
+++ xiaokala/xiaokala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xiaokala
+//
+//  Created by luosi on 2026/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
